14233cec7d49effc6b34e68d436a31a69653fdfe
Ashutosh Sathe <2ashutoshbs@gmail.com>
2021-04-01 15:00:43 +0530

    PoC ready
    Needs a LOTTA cleanup work, but PoC is ready

--- try.py
+++ try.py
@@ -27,21 +27,31 @@ for path,attr in zip(paths, attributes):
         myPathList.append(path.point(path.ilength(pathLength * i / (numSamples-1))))
     myPaths[pathColour] = np.array(myPathList)
 pprint(myPaths)
-
 for k, v in myPaths.items():
     x = [e.real for e in v]
     y = [e.imag for e in v]
     plt.plot(x, y, marker='v', label=k)
 plt.legend()
-plt.show()
-
 # !!!!! WE ARE DONE !!!!!
 # SEE
 # https://stackoverflow.com/questions/22566284/matplotlib-how-to-plot-images-instead-of-points
 # ALSO THIS, ESPECIALLY THIS
 # https://stackoverflow.com/a/53851017/7914234
-
-
+"""
+img_path = './sample_imageone.png'
+file_paths = [img_path for _ in range(numSamples)]
+fig, ax = plt.subplots()
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+for k, v in myPaths.items():
+    x = [e.real for e in v]
+    y = [e.imag for e in v]
+    ax.plot(x, y)
+    for x0, y0, path in zip(x, y, file_paths):
+        ab = AnnotationBbox(OffsetImage(plt.imread(path)), (x0, y0))
+        ax.add_artist(ab)
+"""
+plt.gca().set_aspect('equal', adjustable='box')
+plt.show()
 # !!!!! PARALLELIZING THE CODE !!!!!
 # https://urban-institute.medium.com/using-multiprocessing-to-make-python-code-faster-23ea5ef996ba
 # https://towardsdatascience.com/a-hands-on-guide-to-multiprocessing-in-python-48b59bfcc89e
