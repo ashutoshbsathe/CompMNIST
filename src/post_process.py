27cdf4a99fb6a4118c79c286b0bbc5de3105b811
Ashutosh Sathe <2ashutoshbs@gmail.com>
2021-04-29 17:29:46 +0530

    Update post processing to be more generic

--- src/post_process.py
+++ src/post_process.py
@@ -4,6 +4,7 @@ from multiprocessing import Pool
 from tqdm import tqdm
 from PIL import Image
 target_size = 224
+binarize = False 
 def process_image(file_path: str):
     img = Image.open(file_path)
     w, h = img.size
@@ -11,13 +12,22 @@ def process_image(file_path: str):
     x = Image.new('RGB', (size, size), (255, 255, 255))
     x.paste(img, (int((size - w) / 2), int((size - h) / 2)))
     x = np.array(x.resize((target_size, target_size)).convert('L'))
-    x = (x >= (x.max() + x.min() / 2)).astype(np.bool)
+    if binarize:
+        x = (x >= (x.max() + x.min() / 2)).astype(np.bool)
     return x
 
-def post_process(f: str, output:str):
+def post_process(f: str, output:str, indices=None):
     df = pd.read_csv(f)
-    file_list = df['Output File'].tolist()#[:10]
-    with Pool(processes=8) as p:
+    assert indices is not None
+    print(len(indices))
+    if indices is None:
+        file_list = df['Output File'].tolist()#[:10]
+    else:
+        # file_list = df.iloc[indices, dfd.columns.get_loc('Output File')].tolist()
+        file_list = df.iloc[indices]['Output File'].tolist()
+        print(len(file_list))
+        assert len(file_list) == len(indices)
+    with Pool(processes=4) as p:
         """
         with tqdm(total=10) as pbar:
             pbar.set_description('lol')
